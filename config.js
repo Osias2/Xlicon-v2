09c1abb1103c2be58f98da8c734a8cbee3036043
OSIAS <ostakan07@gmail.com>
2024-07-29 20:19:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ global.Owner = ["8801853262586","8801928502256"];         //If you want singal n
 global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
 global.ownertag = ["8801928502256"];
 global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
+global.BotName = "OSIAS-TECH";
 global.packname = "X-2.0 Bot";                             //Do not change.
 global.author = "By: Slasher";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
